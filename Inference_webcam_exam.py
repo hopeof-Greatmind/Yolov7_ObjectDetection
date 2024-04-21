f8b752abcbd5cac9b8a4719bd776e621cf81a614
GreatMind <bg.kim@sookmyung.ac.kr>
2024-04-21 15:44:53 +0900

    ONNX-baed Object detection

---
+++ Inference_webcam_exam.py
@@ -0,0 +1,146 @@
+# Inference for ONNX model
+
+import cv2
+import tensorflow as tf
+cuda = True
+w = "yolov7-tiny.onnx"
+#img = cv2.imread('horses.jpg')  # image-based execute!
+
+import time
+import requests
+import random
+import numpy as np
+import onnxruntime as ort
+from PIL import Image
+from pathlib import Path
+from collections import OrderedDict,namedtuple
+
+providers = ['CUDAExecutionProvider', 'CPUExecutionProvider'] if cuda else ['CPUExecutionProvider'] #['AzureExecutionProvider', 'CPUExecutionProvider'] if cuda else ['CPUExecutionProvider']
+session = ort.InferenceSession(w, providers=providers)
+
+tf.compat.disable_v2_behavior()
+with tf.compat.Session() as sess:
+    x = tf.compat.placeholder(tf.float32, [2])
+    x2 = tf.square(x)
+    print(sess.run(x2, feed_dict={x: [2, 3]}))
+    # [4. 9.]
+
+#print("00000")
+
+def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleup=True, stride=32):
+    # Resize and pad image while meeting stride-multiple constraints
+    shape = im.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better val mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return im, r, (dw, dh)
+
+names = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 
+         'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 
+         'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 
+         'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 
+         'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 
+         'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 
+         'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 
+         'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 
+         'hair drier', 'toothbrush']
+colors = {name:[random.randint(0, 255) for _ in range(3)] for i,name in enumerate(names)}
+
+webcam = cv2.VideoCapture(0)
+webcam.set(3, 640)
+webcam.set(4, 480)
+ratio_factor = 0.9
+
+if not webcam.isOpened():
+    print("Could not open webcam")
+    exit()
+
+while webcam.isOpened():
+    status, img = webcam.read()
+    
+    height, width = img.shape[:2]
+    img = cv2.resize(img, (1280, 720))
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #cv2.imshow("original",img)
+    
+
+    image = img.copy()
+    image, ratio, dwdh = letterbox(image, auto=False)
+    image = image.transpose((2, 0, 1))
+    image = np.expand_dims(image, 0)
+    image = np.ascontiguousarray(image)
+
+    #print("11111")
+
+    im = image.astype(np.float32)
+    im /= 255
+    im.shape
+
+    outname = [i.name for i in session.get_outputs()]
+    outname
+
+    inname = [i.name for i in session.get_inputs()]
+   # inname
+
+    inp = {inname[0]:im}
+
+    # ONNX inference
+    outputs = session.run(outname, inp)[0]
+    #outputs
+
+    ori_images = [img.copy()]
+
+    for i,(batch_id,x0,y0,x1,y1,cls_id,score) in enumerate(outputs):
+        image = ori_images[int(batch_id)]
+        box = np.array([x0,y0,x1,y1])
+        box -= np.array(dwdh*2)
+        box /= ratio
+        box = box.round().astype(np.int32).tolist()
+        cls_id = int(cls_id)
+        score = round(float(score),3)
+        name = names[cls_id]
+        color = colors[name]
+        name += ' '+str(score)
+        cv2.rectangle(image,box[:2],box[2:],color,2)
+        cv2.putText(image,name,(box[0], box[1] - 2),cv2.FONT_HERSHEY_SIMPLEX,0.75,[225, 255, 255],thickness=2)  
+
+    print('[INFO] draw all detected boxes by Prof. Kim....!')    #<===== 여기에 여러분들의 학번이 표시되도록 합니다.
+
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # PIL image --> opencv image Mat buffer
+    
+    if status:
+        cv2.imshow("results",image)
+    
+    #if status:
+    #    img=Image.fromarray(ori_images[0])
+    #    img.save('output.png')
+    #    img.show()
+    #    cv2.imshow(img)
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
+
